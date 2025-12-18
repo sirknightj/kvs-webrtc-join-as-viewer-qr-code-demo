ce7ec7df2ee63b03f3e705dc8af98cd190c812fd
sirknightj <jggunawa@amazon.com>
2025-12-18 10:41:22 -0800

    Fix the path

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
+  basename: "/kvs-webrtc-join-as-viewer-qr-code-demo",
 } satisfies Config;
